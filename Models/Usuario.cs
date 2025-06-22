b22d41242a5fa829c83dbaeddca602c0ed9cdb72
Henrique Capitani <henriquecapitani.mk@gmail.com>
2025-06-22 20:04:35 -0300

    feat: adiciona model Usuario

---
+++ Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿namespace Itau_invest.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public decimal PercentualCorretagem { get; set; }
+
+        // Construtor padrão 
+        public Usuario() { }
+
+        // Construtor completo 
+        public Usuario(int idUsuario, string nome, string email, decimal percentualCorretagem)
+        {
+            IdUsuario = idUsuario;
+            Nome = nome;
+            Email = email;
+            PercentualCorretagem = percentualCorretagem;
+        }
+    }
+}
